d8cf11887d31486537044d20ed9b25c3e06e9d1d
Peter Turner <git-commit@flintsteel7.com>
2018-01-11 09:50:31 -0800

    set up for Episode 2

---
+++ episode02/README.md
@@ -0,0 +1,3 @@
+## Lua Math: Episode 2 - Intro to Trigonometry
+
+Draw a black sine wave on a white background
---
+++ episode02/main.lua
@@ -0,0 +1,10 @@
+function love.load()
+  title = "Lua Math: Episode 2 - Intro to Trigonometry"
+  window_width = 1200
+  window_height = 800
+
+  love.window.setMode(window_width, window_height)
+  love.window.setTitle(title)
+
+  love.graphics.setBackgroundColor(255, 255, 255)
+end
