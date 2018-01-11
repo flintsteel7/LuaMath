a8c2d7ac02e89497393eaab4ba42bf75bac3600a
Peter Turner <git-commit@flintsteel7.com>
2018-01-11 08:14:12 -0800

    follow Lua practice of 1 as first index

--- Episode 1 - Introduction/main.lua
+++ Episode 1 - Introduction/main.lua
@@ -17,7 +17,7 @@ math.randomseed(os.time())
 -- create a table with 100 sets of begin and end coordinates
 points = {}
 
-for i = 0, 400, 2 do
+for i = 1, 400, 2 do
   points[i] = math.random(0, window_width)
   points[i + 1] = math.random(0, window_height)
 end
@@ -25,7 +25,7 @@ end
 function love.draw()
   -- loop through the table, drawing a line between each set of coordinates
   love.graphics.setColor(0, 0, 0)
-  for i = 0, 400, 4 do
+  for i = 1, 400, 4 do
     love.graphics.line(points[i], points[i + 1], points[i + 2], points[i + 3])
   end
 end
