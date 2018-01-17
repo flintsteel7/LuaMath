d1c317108ef129b26e98d24f6c8851df9f54477b
flintsteel7 <git-commit@flintsteel7.com>
2018-01-16 22:31:56 -0800

    update boilerplate

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
+title = "Lua Math: Boilerplate"
+window_width = 1200
+window_height = 800
+
 function love.load()
-  title = "Lua Math: Boilerplate"
-  window_width = 1200
-  window_height = 800
 
   love.window.setMode(window_width, window_height)
   love.window.setTitle(title)
 
   love.graphics.setBackgroundColor(255, 255, 255)
+  love.graphics.setColor(0, 0, 0)
 end
