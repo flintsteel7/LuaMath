dbd614109f84cf4f67955ec2a549491d66f47b2b
Peter Turner <git-commit@flintsteel7.com>
2018-03-27 22:31:38 -0700

    start coding 'Episode 7 - Vectors Part2'

---
+++ episode07/README.md
@@ -0,0 +1,3 @@
+## Lua Math: Episode 7 - Vectors Part 2
+
+Write a minimal 2D Vector library
---
+++ episode07/vector.lua
@@ -0,0 +1,20 @@
+Vector = {
+  x = 1,
+  y = 0
+}
+
+function Vector.setX(value)
+  Vector.x = value
+end
+
+function Vector.getX()
+  return Vector.x
+end
+
+function Vector.setY(value)
+  Vector.y = value
+end
+
+function Vector.getY()
+  return Vector.y
+end
